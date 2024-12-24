09710beaf2f58a9e6d008025f8a0a34b64a00407
Krish Kalla <krish.k.2626@gmail.com>
2024-12-23 21:57:53 -0500

    Added embeding fetching code and embeddings to JSON

--- main.py
+++ main.py
@@ -3,13 +3,21 @@ import json
 from tqdm import tqdm
 
 responses = []
-iterations = 20
+embeddings = []
+iterations = 3
 
-DEBUG = False
+DEBUG = 1
 
-def debug(*args, **kwargs):
-    if DEBUG:
+def debug(level, *args, **kwargs):
+    if DEBUG == 1 and level >= 1:
         print(*args, **kwargs)
+    if DEBUG == 2 and level == 2:
+        print(*args, **kwargs)
+    if DEBUG == 3 and level == 3:
+        print(*args, **kwargs)
+    if DEBUG == 4:
+        print(*args, **kwargs)
+
 
 client = OpenAI()
 def get_response():
@@ -27,26 +35,41 @@ def get_response():
             } 
         ]
     )
+    debug(2, completion)
     return completion
+
+def get_embedding(text):
+    embedding = client.embeddings.create(
+        model = 'text-embedding-ada-002',
+        input = text,
+        encoding_format = 'float'
+    )
+    debug(3, embedding)
+    return embedding.data[0].embedding
 
 # Gather responses from ChatGPT
 for i in tqdm(range(iterations)):
     completion = get_response()
     response = completion.choices[0].message.content
-    debug('Unparsed Response {i}: ', response)
+    embedding = get_embedding(response)
+    debug(1, 'Unparsed Response {i}: ', response)
+    debug(1, 'Embedding fetched {i}')
     #add Intelligence is to the beginning of the response if not already present
     response = response.strip()
     response = response.lower()
     if not response.startswith('intelligence is'):
         response = 'intelligence is ' + response
-    debug('Parsed Response {i}: ', response)
+    debug(1, 'Parsed Response {i}: ', response)
     responses.append(response)
+    embeddings.append(embedding)
+    debug(1, 'Appended responses and embeddings')
 
 data = {
-    'sentences': responses
+    'sentences': responses,
+    'embeddings': embeddings
 }
 
-debug('data dictionary created')
+debug(1, 'data dictionary created')
 
 with open("responses.json", 'w', encoding = 'utf-8') as f:
     json.dump(data, f, ensure_ascii = False, indent = 4)
